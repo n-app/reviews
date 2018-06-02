be6e6cdae85f30167e4dba6e9cd7f2a82f71b575
clfhhc <clfhhc@gmail.com>
2018-06-01 21:17:05 -0700

    setup server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(express.static('public'));
+
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+const server = http.createServer(app);
+server.listen(3003);
