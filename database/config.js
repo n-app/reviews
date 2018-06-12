cbe10e8de51cfc983cd8fd7cacc3e07f1f590acf
clfhhc <clfhhc@gmail.com>
2018-06-12 14:47:53 -0700

    added databas support to circle ci

---
+++ database/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  user: 'root',
+  host: '127.0.0.1',
+  // password: 'YOUR_PASSWORD_HERE',
+  // if you do not have a password, delete this key/value pair from
+  // the object
+  database: 'nappbnbreviews',
+  multipleStatements: true,
+};
